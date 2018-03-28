b20ebec8e1bbb8e3aa9a6758926a087b2f7ccb00
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-28 17:42:29 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,11 +30,11 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990} )
 end
 
 def can_get_the_first_item_in_the_database
-  binding.pry
+  Movie.first.title
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  binding.pry
 end
 
 def can_get_size_of_the_database
